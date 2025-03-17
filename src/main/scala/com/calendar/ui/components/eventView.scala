8d0ecd48571ade59c1ad303710813ba7a02e7607
naatti16 <148633784+anttitulenheimo@users.noreply.github.com>
2025-03-17 20:07:50 +0200

    Implemented eventView and added a new constant
    eventView now has basic structure to show event and it's details

--- src/main/scala/com/calendar/ui/components/eventView.scala
+++ src/main/scala/com/calendar/ui/components/eventView.scala
@@ -1,3 +1,71 @@
 package com.calendar.ui.components
 
-object eventView
+import com.calendar.models.Event
+import com.calendar.ui.constants
+import scalafx.geometry.Pos.Center
+import scalafx.scene.control.Label
+import scalafx.scene.layout.VBox
+import scalafx.scene.paint.Color
+import scalafx.scene.text.{ Font, FontWeight }
+
+import java.time.format.DateTimeFormatter
+import scala.util.{ Failure, Success, Try }
+
+object eventView {
+
+  val eventHeadingfontSize = constants.windowWidth * 0.01
+  val eventNormalFont = constants.windowWidth * 0.008
+
+  // Formats the given inputs
+  private val dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+  private val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
+
+  // Creates an event display
+  def createEventDisplay(event: Event): VBox = {
+    // Tries to set a background color otherwise sets a default color
+    val background =
+      Try(Color.web(event.colorCode)) match
+        case Success(realColor) =>
+          realColor
+        case Failure(exception) =>
+          constants.eventDefaultColor
+
+    // Creates an event box where event's parameters are shown
+    val eventBox = new VBox() {
+      alignment = Center
+    }
+
+    // Displays event's name
+    val eventHeading = new Label(event.name) {
+      font = Font.font("Montserrat", FontWeight.Bold, eventHeadingfontSize)
+    }
+
+    // Displays timedata
+    val timeLabel = new Label(
+      s"${event.startingTime.format(timeFormatter)} - ${event.endingTime.format(timeFormatter)}"
+    ) {
+      font = Font.font("Montserrat", FontWeight.Light, eventNormalFont)
+    }
+
+    // Displays category name
+    val categoryLabel = new Label(event.category.name) {
+      font = Font.font("Montserrat", FontWeight.Light, eventNormalFont)
+    }
+
+    // Displays additional info
+    val additionalInfoLabel = event.additionalInfo match
+      case Some(additionalInfo) =>
+        new Label(additionalInfo) {
+          font = Font.font("Montserrat", FontWeight.Light, eventNormalFont)
+        }
+      case _ =>
+        new Label("No additional info") {
+          font = Font.font("Montserrat", FontWeight.Light, eventNormalFont)
+        }
+
+    eventBox.children.addAll(eventHeading, timeLabel, categoryLabel)
+    eventBox
+
+  }
+
+}
--- src/main/scala/com/calendar/ui/constants.scala
+++ src/main/scala/com/calendar/ui/constants.scala
@@ -1,7 +1,7 @@
 package com.calendar.ui
 
 import scalafx.stage.Screen
-
+import scalafx.scene.paint.Color
 //Constants object for the UI components to use
 
 object constants {
@@ -10,5 +10,8 @@ object constants {
     Screen.primary.visualBounds // Returns the primary screenbounds
   val windowWidth = screenBounds.width * 0.8 // Scaled
   val windowHeight = screenBounds.height * 0.8 // Scaled
+  
+  val eventDefaultColor = Color.LightGreen
+  
 
 }
