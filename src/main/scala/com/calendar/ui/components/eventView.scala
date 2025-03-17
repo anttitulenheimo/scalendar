b6a9a3d9842e6be69ffee4fb1515233321029295
naatti16 <148633784+anttitulenheimo@users.noreply.github.com>
2025-03-17 15:51:53 +0200

    Created new class eventView and constants object
    eventView will be a new component to show the events in calendar and the constants makes the code simpler

---
+++ src/main/scala/com/calendar/ui/components/eventView.scala
@@ -0,0 +1,3 @@
+package com.calendar.ui.components
+
+object eventView
---
+++ src/main/scala/com/calendar/ui/constants.scala
@@ -0,0 +1,14 @@
+package com.calendar.ui
+
+import scalafx.stage.Screen
+
+//Constants object for the UI components to use
+
+object constants {
+
+  val screenBounds =
+    Screen.primary.visualBounds // Returns the primary screenbounds
+  val windowWidth = screenBounds.width * 0.8 // Scaled
+  val windowHeight = screenBounds.height * 0.8 // Scaled
+
+}
